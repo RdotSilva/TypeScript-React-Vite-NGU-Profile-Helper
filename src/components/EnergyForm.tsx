a18e52eea7b0487e4d3cd2feca10385f38a841ae
RdotSilva <RyandotSilva@gmail.com>
2024-04-15 19:11:28 +0100

    Add radio example

--- src/components/EnergyForm.tsx
+++ src/components/EnergyForm.tsx
@@ -1,4 +1,4 @@
-import { Select, Input, Button } from "antd";
+import { Select, Input, Button, Radio } from "antd";
 
 import { useForm, Controller } from "react-hook-form";
 
@@ -35,7 +35,20 @@ const EnergyForm = () => {
         Submit
       </Button>
 
-      {/* <input type="submit" /> */}
+      <Controller
+        render={({ field }) => (
+          <Radio.Group
+            {...field}
+            options={[
+              { value: "True", label: "True" },
+              { value: "False", label: "False" },
+            ]}
+          />
+        )}
+        control={control}
+        name="capAllBtSelect"
+        defaultValue={"True"}
+      />
     </form>
   );
 };
