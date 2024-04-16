647a60a7832e9d684d4985049f381c911c0a0cfd
RdotSilva <RyandotSilva@gmail.com>
2024-04-16 18:47:15 +0100

    Add RadioForm component

---
+++ src/components/RadioForm.tsx
@@ -0,0 +1,40 @@
+import { Button, Radio } from "antd";
+
+import { useForm, Controller } from "react-hook-form";
+
+const defaultValues = {
+  capAllBtSelect: "True",
+};
+
+const RadioForm = () => {
+  const { handleSubmit, reset, watch, control, register } = useForm({
+    defaultValues,
+  });
+
+  const onSubmit = (data) => console.log(data);
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Controller
+        render={({ field }) => (
+          <Radio.Group
+            {...field}
+            options={[
+              { value: "True", label: "True" },
+              { value: "False", label: "False" },
+            ]}
+          />
+        )}
+        control={control}
+        name="capAllBtSelect"
+        defaultValue={"True"}
+      />
+
+      <Button type="primary" htmlType="submit">
+        Submit
+      </Button>
+    </form>
+  );
+};
+
+export default RadioForm;
