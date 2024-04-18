e38672bbfbf6ebc271a688d7e5dddb13fb3521f2
RdotSilva <RyandotSilva@gmail.com>
2024-04-18 18:39:13 +0100

    Add TSDoc comments

--- src/utils/generator.ts
+++ src/utils/generator.ts
@@ -1,3 +1,8 @@
+/**
+ * Generate JSON based on the incoming data from the form
+ * @param value Value of the CAPALLBT data
+ * @returns JSON data that can be imported into the injector
+ */
 const generateJsonObject = (value: string) => {
   if (value === "True") {
     return {
