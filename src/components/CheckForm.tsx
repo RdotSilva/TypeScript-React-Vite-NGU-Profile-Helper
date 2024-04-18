41920efd06f7992cb31ab87d4d0d755772a654b6
RdotSilva <RyandotSilva@gmail.com>
2024-04-18 18:37:22 +0100

    Add CheckForm component

---
+++ src/components/CheckForm.tsx
@@ -0,0 +1,39 @@
+import { Button, Checkbox } from "antd";
+import { useForm } from "react-hook-form";
+import generateJsonObject from "../utils/generator";
+
+const defaultValues = {
+  CAPALLBT: true,
+};
+
+const CheckForm = () => {
+  const { handleSubmit, control } = useForm({
+    defaultValues,
+  });
+
+  const onSubmit = (data) => {
+    const capallbtValue = data.CAPALLBT;
+
+    const valueString = capallbtValue ? "True" : "False";
+
+    const jsonObject = generateJsonObject(valueString);
+    console.log(jsonObject);
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Checkbox
+        {...control.register("CAPALLBT")}
+        defaultChecked={defaultValues.CAPALLBT}
+      >
+        CAPALLBT
+      </Checkbox>
+
+      <Button type="primary" htmlType="submit">
+        Submit
+      </Button>
+    </form>
+  );
+};
+
+export default CheckForm;
