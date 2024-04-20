b5d459e37da1306eb3ba366f3be7983c19b595fa
RdotSilva <RyandotSilva@gmail.com>
2024-04-20 19:35:19 +0100

    Add MainForm component

---
+++ src/components/MainForm.tsx
@@ -0,0 +1,12 @@
+import { Flex } from "antd";
+import CheckForm from "./CheckForm";
+
+const MainForm = () => {
+  return (
+    <Flex>
+      <CheckForm />
+    </Flex>
+  );
+};
+
+export default MainForm;
