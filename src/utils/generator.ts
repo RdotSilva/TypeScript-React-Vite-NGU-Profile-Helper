891420314aefb4a808ec7328031c08f9b83f1f9b
RdotSilva <RyandotSilva@gmail.com>
2024-05-02 17:34:46 +0100

    Refactor generateJsonObject function

--- src/utils/generator.ts
+++ src/utils/generator.ts
@@ -1,26 +1,15 @@
-export interface FormData {
-  CAPALLBT: string;
-}
-
 /**
  * Generate JSON based on the incoming data from the form
  * @param data Incoming form data
  * @returns JSON data that can be imported into the injector
  */
-const generateJsonObject = (data: FormData) => {
-  const { CAPALLBT } = data;
+const generateJsonObject = (data: Record<string, boolean>) => {
+  console.log(`Generate json data: ${JSON.stringify(data)}`);
 
-  if (CAPALLBT) {
-    return {
-      Time: 0,
-      Priorities: ["CAPALLBT"],
-    };
-  } else {
-    return {
-      Time: 0,
-      Priorities: [],
-    };
-  }
+  return {
+    Time: 0,
+    Priorities: generateEnergyPriorities(data),
+  };
 };
 
 export default generateJsonObject;
