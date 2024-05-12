3caad5a87f0b4cadfaa6eecbed0b5121c3464216
RdotSilva <RyandotSilva@gmail.com>
2024-05-12 20:33:13 +0100

    Add FormProvider to MainForm

--- src/components/MainForm/MainForm.tsx
+++ src/components/MainForm/MainForm.tsx
@@ -1,12 +1,29 @@
 import { Flex } from "antd";
 import CheckForm from "../CheckForm/CheckForm";
 import "./MainForm.css";
+import { useForm, FormProvider, useFormContext } from "react-hook-form";
+import { EnergyFormValues } from "../../types/energy/energyTypes";
+
+const defaultValues = {
+  CAPALLBT: true,
+  CAPALLAT: true,
+  CAPALLNGU: true,
+  CAPBESTAUG: true,
+  CAPWAN: true,
+  CAPTM: true,
+};
 
 const MainForm = () => {
+  const methods = useForm<EnergyFormValues>({
+    defaultValues,
+  });
+
   return (
-    <Flex className="form-container">
-      <CheckForm />
-    </Flex>
+    <FormProvider {...methods}>
+      <Flex className="form-container">
+        <CheckForm />
+      </Flex>
+    </FormProvider>
   );
 };
 
