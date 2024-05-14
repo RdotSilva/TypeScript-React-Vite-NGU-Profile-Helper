1ed41c582011da43bac3c03df1932e63cf00ac5c
RdotSilva <RyandotSilva@gmail.com>
2024-05-14 18:30:43 +0100

    Add TimeSelectForm component

---
+++ src/components/TimeSelectForm/TimeSelectForm.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import type { InputNumberProps } from "antd";
+import { InputNumber } from "antd";
+
+const onChange: InputNumberProps["onChange"] = (value) => {
+  console.log("changed", value);
+};
+
+const TimeSelectForm: React.FC = () => (
+  <InputNumber min={1} max={10} defaultValue={3} onChange={onChange} />
+);
+
+export default TimeSelectForm;
