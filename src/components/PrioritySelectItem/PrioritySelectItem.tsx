ae9ec355db6edf1c5ce749164ffa7b878b1a69da
RdotSilva <RyandotSilva@gmail.com>
2024-06-01 20:51:13 +0100

    Refactor PrioritySelectItem component

--- src/components/PrioritySelectItem/PrioritySelectItem.tsx
+++ src/components/PrioritySelectItem/PrioritySelectItem.tsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 
-import { InputNumber } from "antd";
+import { Select } from "antd";
 import { Control, Controller } from "react-hook-form";
 import { FormValues } from "../../types/energy/energyTypes";
 
@@ -10,27 +10,33 @@ interface TimeSelectFormProps {
   defaultValue: number;
 }
 
-const PrioritySelectItem = ({
-  control,
-  name,
-  defaultValue,
-}: TimeSelectFormProps) => (
-  <>
+const OPTIONS = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const PrioritySelectItem = ({ control, name }: TimeSelectFormProps) => {
+  const [selectedItems, setSelectedItems] = useState<string[]>([]);
+
+  const filteredOptions = OPTIONS.filter((o) => !selectedItems.includes(o));
+
+  return (
     <Controller
       render={({ field }) => (
-        <InputNumber
-          size="small"
-          min={0}
-          max={7}
-          defaultValue={defaultValue}
-          onChange={field.onChange}
-          addonAfter={name}
+        <Select
+          mode="multiple"
+          maxCount={1}
+          placeholder="Select Priority"
+          value={selectedItems}
+          onChange={setSelectedItems}
+          style={{ width: "15%" }}
+          options={filteredOptions.map((item) => ({
+            value: item,
+            label: item,
+          }))}
         />
       )}
       control={control}
       name={name}
     />
-  </>
-);
+  );
+};
 
 export default PrioritySelectItem;
