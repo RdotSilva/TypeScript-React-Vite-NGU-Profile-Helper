0d0c92295218db538c5e4543e632c9bed8da3439
RdotSilva <RyandotSilva@gmail.com>
2024-06-10 20:16:24 +0100

    Add HourSelectForm component

---
+++ src/components/TimeSelectForm/HourSelectForm.tsx
@@ -0,0 +1,22 @@
+import { useFormContext } from "react-hook-form";
+import { InputNumber } from "antd";
+import { FormItem } from "react-hook-form-antd";
+import { TimerOptions } from "../../types/energy/energyTypes";
+
+const HourSelectForm = () => {
+  const { control } = useFormContext();
+
+  return (
+    <>
+      <FormItem
+        control={control}
+        name={TimerOptions.HOURS}
+        label={TimerOptions.HOURS}
+      >
+        <InputNumber />
+      </FormItem>
+    </>
+  );
+};
+
+export default HourSelectForm;
