262c4e539462e1954753ff1a2c06e0e429019f0c
RdotSilva <RyandotSilva@gmail.com>
2024-06-13 17:42:58 +0100

    Add test select item

--- src/components/EnergyForm/EnergyForm.tsx
+++ src/components/EnergyForm/EnergyForm.tsx
@@ -1,22 +1,26 @@
 import { useFormContext } from "react-hook-form";
-import { Checkbox } from "antd";
+import { Checkbox, Flex } from "antd";
 
 import { FormItem } from "react-hook-form-antd";
 
 import { EnergyCapFieldNames } from "../../types/energy/energyTypes";
+import PrioritySelectItem from "../PrioritySelectItem/PrioritySelectItem";
 
 const EnergyForm = () => {
   const { control } = useFormContext();
 
   return (
     <>
-      <FormItem
-        control={control}
-        name={EnergyCapFieldNames.CAPTM}
-        label={EnergyCapFieldNames.CAPTM}
-      >
-        <Checkbox />/
-      </FormItem>
+      <Flex>
+        <FormItem
+          control={control}
+          name={EnergyCapFieldNames.CAPTM}
+          label={EnergyCapFieldNames.CAPTM}
+        >
+          <Checkbox />/
+        </FormItem>
+        <PrioritySelectItem name="test" label="test" />
+      </Flex>
       <FormItem
         control={control}
         name={EnergyCapFieldNames.CAPALLNGU}
