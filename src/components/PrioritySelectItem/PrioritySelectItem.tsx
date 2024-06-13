e6bbccf6a4e3c4109f8920330ceb224bf17f5bf3
RdotSilva <RyandotSilva@gmail.com>
2024-06-13 17:39:23 +0100

    Add PrioritySelectItem component

---
+++ src/components/PrioritySelectItem/PrioritySelectItem.tsx
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import { Select } from "antd";
+import { FormItem } from "react-hook-form-antd";
+import { useFormContext } from "react-hook-form";
+
+const OPTIONS = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const PrioritySelectItem = ({ name, label }) => {
+  const [selectedItems, setSelectedItems] = useState<string[]>([]);
+  const [openSelect, setOpenSelect] = useState(false);
+  const filteredOptions = OPTIONS.filter((o) => !selectedItems.includes(o));
+
+  const { control } = useFormContext();
+
+  return (
+    <>
+      <FormItem control={control} name={name} label={label}>
+        <Select />/
+      </FormItem>
+    </>
+  );
+};
+
+export default PrioritySelectItem;
