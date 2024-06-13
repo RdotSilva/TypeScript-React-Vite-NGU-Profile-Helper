0a02128befa60197d8b70a5c9e1363e84fbfe4ac
RdotSilva <RyandotSilva@gmail.com>
2024-06-13 17:39:34 +0100

    Add PrioritySelectItemProps interface

--- src/components/PrioritySelectItem/PrioritySelectItem.tsx
+++ src/components/PrioritySelectItem/PrioritySelectItem.tsx
@@ -5,7 +5,12 @@ import { useFormContext } from "react-hook-form";
 
 const OPTIONS = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-const PrioritySelectItem = ({ name, label }) => {
+interface PrioritySelectItemProps {
+  name: string;
+  label: string;
+}
+
+const PrioritySelectItem = ({ name, label }: PrioritySelectItemProps) => {
   const [selectedItems, setSelectedItems] = useState<string[]>([]);
   const [openSelect, setOpenSelect] = useState(false);
   const filteredOptions = OPTIONS.filter((o) => !selectedItems.includes(o));
