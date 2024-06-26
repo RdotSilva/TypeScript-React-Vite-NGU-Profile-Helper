4dac513cd2a21b66e5205c2125cb9e99e8f03ae6
RdotSilva <RyandotSilva@gmail.com>
2024-06-26 20:43:37 +0100

    Add input for magic CAPALLNGU

--- src/components/MagicForm/MagicForm.tsx
+++ src/components/MagicForm/MagicForm.tsx
@@ -1,9 +1,23 @@
+import { Checkbox, Flex } from "antd";
 import { useFormContext } from "react-hook-form";
+import { FormItem } from "react-hook-form-antd";
+import { MagicCap } from "../../types/magic/magicTypes";
 
 const MagicForm = () => {
   const { control } = useFormContext();
 
-  return <>Magic Form</>;
+  return (
+    <Flex>
+      <FormItem
+        valuePropName="checked"
+        control={control}
+        name={MagicCap.CAPALLNGU.name}
+        label={MagicCap.CAPALLNGU.name}
+      >
+        <Checkbox />/
+      </FormItem>
+    </Flex>
+  );
 };
 
 export default MagicForm;
