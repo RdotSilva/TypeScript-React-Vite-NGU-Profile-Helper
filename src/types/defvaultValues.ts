c049dfb28811bc2d899ed9d9ed6ad0b13e487120
RdotSilva <RyandotSilva@gmail.com>
2024-07-03 18:36:26 -0400

    Update defaultValues with new values

--- src/types/defvaultValues.ts
+++ src/types/defvaultValues.ts
@@ -1,13 +1,14 @@
+export const energyDefaultValues = {
+  ENERGY_CAPTM: true,
+};
+
+export const magicDefaultValues = {
+  MAGIC_CAPTM: true,
+};
+
 export const defaultValues = {
   HOURS: 0,
   MINUTES: 1,
-  CAPTM: true,
+  ...energyDefaultValues,
+  ...magicDefaultValues,
 };
-
-export const energyDefaultValues = {
-  ENERGY_CAPTM: true
-}
-
-export const magicDefaultValues = {
-  MAGIC_CAPTM: true
-}
