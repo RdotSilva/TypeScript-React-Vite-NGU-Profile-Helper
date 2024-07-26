c4cb14d73100410c2e5c5c1b2c1e6cf6576e5d8d
RdotSilva <RyandotSilva@gmail.com>
2024-07-26 11:10:56 -0400

    Fix old imports

--- src/components/archived/EnergyFormOld.tsx
+++ src/components/archived/EnergyFormOld.tsx
@@ -6,7 +6,6 @@ import {
   EnergyCapFieldNames,
   EnergyCapFieldPriorities,
   FormValues,
-  TimerOptions,
 } from "../../types/energy/energyTypes";
 import CheckFormItem from "./CheckFormItem/CheckFormItem";
 import SelectFormItem from "./SelectFormItem";
@@ -15,6 +14,7 @@ import { useState } from "react";
 
 import { CopyToClipboard } from "react-copy-to-clipboard";
 import PrioritySelectItem from "./PrioritySelectItem";
+import { TimerOptions } from "../../types/timer/timerTypes";
 
 interface EnergyFormProps {
   defaultValues: {
