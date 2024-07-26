715049b99bbbc83189ff48ba04ba9f57cbc8a6ac
RdotSilva <RyandotSilva@gmail.com>
2024-07-25 21:29:17 -0400

    Fixer timer option import

--- src/components/TimeSelectForm/SecondSelectForm.tsx
+++ src/components/TimeSelectForm/SecondSelectForm.tsx
@@ -1,7 +1,7 @@
 import { useFormContext } from "react-hook-form";
 import { InputNumber } from "antd";
 import { FormItem } from "react-hook-form-antd";
-import { TimerOptions } from "../../types/energy/energyTypes";
+import { TimerOptions } from "../../types/timer/timerTypes";
 
 const SecondSelectForm = () => {
   const { control } = useFormContext();
@@ -10,8 +10,8 @@ const SecondSelectForm = () => {
     <>
       <FormItem
         control={control}
-        name={TimerOptions.MINUTES}
-        label={TimerOptions.MINUTES}
+        name={TimerOptions.SECONDS}
+        label={TimerOptions.SECONDS}
       >
         <InputNumber min={0} max={59} />
       </FormItem>
